db3e10a4cd422b412b17170d0014c8d3de62046a
HJ-C <swch56@naver.com>
2023-01-23 23:10:51 +0900

    8강 DFS

---
+++ 8강/8-8.js
@@ -0,0 +1,23 @@
+// DFS vs For문
+
+function solution(n, m){
+    let answer=[];
+    let tmp = Array.from({length:m}, ()=>0);
+    console.log(tmp)
+
+    function DFS(L){
+        if(L === m){
+            answer.push(tmp.slice()) //slice()내부가 기본문자열일때 깊은복사 느낌으로 많이 쓰임
+        }else{
+            for(let i=0; i<=n; i++){ //3갈래로 나뉨, 
+                tmp[L]=i; //1,2,3중 포함되면 tmp에 1 적립
+                DFS(L+1); //다음 레벨로 변경
+            }
+        }
+    }
+    
+    DFS(0);
+    return answer;
+}
+
+console.log(solution(3, 2));
